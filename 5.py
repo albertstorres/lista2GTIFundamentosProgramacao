00e4a9a18c9fe65122491673c0f506b7753ace63
Albert Sevy <albertstorres@gmail.com>
2024-03-08 17:00:43 -0300

    Exercício 5 com validação das entradas (gols)

--- 5.py
+++ 5.py
@@ -4,11 +4,16 @@
 time1 = input("Digite o nome do time mandante: ")
 time2 = input("Digite o nome do time visitante: ")
 gols_time1 = int(input(f"Digite o número de gols {time1}: "))
+while gols_time1 < 0 :
+    gols_time1 = int(input(f"Digite o número de gols do {time1}: "))
 gols_time2 = int(input(f"Digite o número de gols do {time2}: "))
+while gols_time2 < 0 :
+    gols_time2 = int(input(f"Digite o número de gols do {time2}: "))
 
 if gols_time1 > gols_time2 :
-    print(time1)
+    print(f"Vencedor: {time1}")
 elif gols_time2 > gols_time1 :
-    print(time2)
+    print(f"Vencedor: {time2}")
 else :
     print("EMPATE")
+
