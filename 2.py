64d1c637d991b50bf05e30c83b0cf4e9a23c5d43
Albert Sevy <albertstorres@gmail.com>
2024-03-08 16:46:09 -0300

    Exercício 2 com validação das entradas

--- 2.py
+++ 2.py
@@ -5,9 +5,16 @@
 # "Reporvado por média".
 # Mas, se a frequência for abaixo de 75%, apresenta a mensagem "Reprovado por falta"
 
-nota1 = float(input("Digite a nota 1: "))
-nota2 = float(input("Digite a nota 2: "))
+nota1 = float(input("Digite a primeira nota: "))
+while nota1 < 0 or nota1 > 10 :
+    nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+while nota2 < 0 or nota2 > 10 :
+    nota2 = float(input("Digite a segunda nota: "))
+
 frequencia = float(input("Digite a frequência do aluno: "))
+while frequencia < 0 or frequencia > 100 :
+    frequencia = float(input("Digite a frequência do aluno: "))
 
 if frequencia >= 75 :
     media = (nota1 + nota2) / 2
